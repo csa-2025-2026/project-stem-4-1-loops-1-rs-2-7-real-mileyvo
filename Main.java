580baf7e3b1042fe5e8de33bf61608b13c552f6c
mileyvo <mtvo106@student.hbuhsd.edu>
2025-10-17 18:20:36 +0000

    10/17/2025

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    // write a loop that prints the odd numbers between 3 and 25
+
+    int count = 1;
+    while (count <= 24)
+    {
+      count += 2;
+      System.out.println(count);
+    }
   }
 }
